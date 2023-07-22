4554f67c81c155912b121a0e2c6b50287dd007ce
AmyMaton <amymaton24@gmail.com>
2023-07-22 11:51:00 +0100

    🧟 max enemies at once and per wave

--- MercuryJam/Assets/Scripts/Enemies/SpawningSystem.cs
+++ MercuryJam/Assets/Scripts/Enemies/SpawningSystem.cs
@@ -1,20 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SpawningSystem : MonoBehaviour
 {
     public List<GameObject> SpawnPoints;
 
     public GameObject Enemy;
+    private List<GameObject> _enemies = new List<GameObject>();
+    public int MaxEnemies = 5;
+    private int _enemiesThisWave = 0;
+    private int _maxEnemiesThisWave = 10;
 
-    private void Start()
+    public void Start()
     {
-        for (int i = 0; i < SpawnPoints.Count; i++)
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (true)
         {
-            Instantiate(Enemy, SpawnPoints[i].transform.position, Quaternion.identity);
+            if (_enemies.Count < MaxEnemies && _enemiesThisWave < _maxEnemiesThisWave)
+            {
+                GameObject NewEnemy = Instantiate(Enemy, SpawnPoints[Random.Range(0, SpawnPoints.Count)].transform.position, Quaternion.identity);
+                _enemies.Add(NewEnemy);
+                _enemiesThisWave++;
+            }
+            yield return new WaitForSeconds(1.0f);
         }
     }
 }
+
 
    
