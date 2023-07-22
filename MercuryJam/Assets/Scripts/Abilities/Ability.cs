90381679883817ed21372b7ada3d36832248b97d
Maria <25516941+m5ka@users.noreply.github.com>
2023-07-22 13:49:04 +0100

    🔥 add basic ability spawning functionality

---
+++ MercuryJam/Assets/Scripts/Abilities/Ability.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Ability : MonoBehaviour
+{
+    public float cooldown = 2.0f;
+    public float speed = 7.0f;
+    public float maxDistance = 24.0f;
+
+    private Transform _transform;
+    private Vector3 _startPoint;
+    private Vector3 _direction;
+
+    public void Initiate(Vector3 startPoint, Vector3 direction)
+    {
+        _startPoint = startPoint;
+        _transform.position = _startPoint;
+        _direction = direction;
+    }
+
+    public void Awake()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    public void Update()
+    {
+        _transform.Translate(_direction * (speed * Time.deltaTime));
+        if (Vector3.Distance(_transform.position, _startPoint) >= maxDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
