43d73203213a55ba40679d1e5c888aac7a8d1d83
Maria <25516941+m5ka@users.noreply.github.com>
2023-07-23 16:35:48 +0100

    💀 fix enemy collision issues

--- MercuryJam/Assets/Scripts/Enemies/EnemyStalking.cs
+++ MercuryJam/Assets/Scripts/Enemies/EnemyStalking.cs
@@ -6,31 +6,37 @@ public class EnemyStalking : MonoBehaviour
 {
     public Animator animator;
     public Enemy enemy;
-    public float speed = 2.0f;
+    public float minSpeed = 1.0f;
+    public float maxSpeed = 2.5f;
     public float targetDistance = 3.0f;
     
-    private Transform _transform;
+    private Rigidbody _rb;
+    private float _speed;
 
     public void Start()
     {
-        _transform = GetComponent<Transform>();
+        _rb = GetComponent<Rigidbody>();
+        _speed = Random.Range(minSpeed, maxSpeed);
     }
     
-    public void Update()
+    public void FixedUpdate()
     {
         if (Player.CurrentPlayer is null)
             return;
         if (enemy.Dead)
             return;
+        
         var playerPosition = new Vector3(Player.CurrentPlayer.Position.x, 0, Player.CurrentPlayer.Position.z);
-        _transform.rotation = Quaternion.LookRotation(playerPosition - _transform.position, Vector3.up);
-        if (Vector3.Distance(playerPosition, _transform.position) > targetDistance)
+        var position = new Vector3(transform.position.x, 0, transform.position.z);
+        
+        _rb.MoveRotation(Quaternion.LookRotation(playerPosition - position, Vector3.up));
+        if (Vector3.Distance(position, Player.CurrentPlayer.Position) > targetDistance)
         {
             animator.SetBool("IsMoving", true);
-            var position = Vector3.MoveTowards(_transform.position, playerPosition, speed * Time.deltaTime);
-            _transform.position = new Vector3(position.x, 0, position.z);
+            _rb.MovePosition(Vector3.MoveTowards(position, playerPosition, _speed * Time.fixedDeltaTime));
             return;
         }
+        
         animator.SetBool("IsMoving", false);
     }
 }
