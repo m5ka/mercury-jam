a7bd9327a1f2a9bb7a7b7bab72bbaf241cf7c359
Maria <25516941+m5ka@users.noreply.github.com>
2023-07-24 19:18:00 +0100

    🐒 change movement input to mouse instead of keyboard

--- MercuryJam/Assets/Scripts/Player/PlayerController.cs
+++ MercuryJam/Assets/Scripts/Player/PlayerController.cs
@@ -1,37 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public Animator playerAnimator;
     public Transform playerRotation;
+    public LayerMask movementLayerMask;
     public float movementSpeed = 5.0f;
     public float rotationSpeed = 720f;
 
     private CharacterController _controller;
     private Transform _transform;
 
-    public void Start()
+    private void Start()
     {
         _controller = GetComponent<CharacterController>();
         _transform = GetComponent<Transform>();
     }
 
-    public void Update()
+    private void Update()
     {
         if (Player.CurrentPlayer is null || Player.CurrentPlayer.Dead)
             return;
         
-        var direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hit, movementLayerMask))
+            return;
+
+        var direction = hit.point - _transform.position;
+        direction.y = 0;
         direction.Normalize();
-        _controller.Move(direction * (movementSpeed * Time.deltaTime));
-        playerAnimator.SetBool("IsMoving", direction != Vector3.zero);
-        if (direction != Vector3.zero)
+
+        playerRotation.rotation = Quaternion.LookRotation(direction, Vector3.up);
+
+        if (Input.GetButton("Move"))
         {
-            var rotation = Quaternion.LookRotation(direction, Vector3.up);
-            playerRotation.rotation =
-                Quaternion.RotateTowards(playerRotation.rotation, rotation, rotationSpeed * Time.deltaTime);
+            playerAnimator.SetBool("IsMoving", true);
+            _controller.Move(direction * (movementSpeed * Time.deltaTime));
+            return;
         }
+        playerAnimator.SetBool("IsMoving", false);
     }
 }
