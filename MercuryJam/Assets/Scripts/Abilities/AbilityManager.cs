334d20d13d6e09146c4355a8ad270e3f711bcbdf
Maria <25516941+m5ka@users.noreply.github.com>
2023-07-24 11:37:34 +0100

    🐸 stop player from hitting after death

--- MercuryJam/Assets/Scripts/Abilities/AbilityManager.cs
+++ MercuryJam/Assets/Scripts/Abilities/AbilityManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 [HideMonoScript]
 public class AbilityManager : MonoBehaviour
@@ -32,6 +33,9 @@ public class AbilityManager : MonoBehaviour
 
     public void Update()
     {
+        if (Player.CurrentPlayer is null || Player.CurrentPlayer.Dead)
+            return;
+        
         if (_delayStarted)
             return;
 
