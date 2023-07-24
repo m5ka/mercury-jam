4314790f9f64a61d5d4d8932852f6e78accb61fb
Maria <25516941+m5ka@users.noreply.github.com>
2023-07-24 13:51:43 +0100

    🔊 update menu scene to use own sound system

--- MercuryJam/Assets/Scripts/UI/MainMenu.cs
+++ MercuryJam/Assets/Scripts/UI/MainMenu.cs
@@ -7,10 +7,14 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public Button startButton;
+    public AudioSource menuMusic;
+    public float musicVolume = 0.2f;
 
     private void Start()
     {
         startButton.onClick.AddListener(TaskOnClick);
+        menuMusic.volume = musicVolume;
+        menuMusic.Play();
     }
 
     private void TaskOnClick()
