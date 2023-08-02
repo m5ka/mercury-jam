fefdba883ce3167a58dc44a2698cd679aae97613
AmyMaton <amymaton24@gmail.com>
2023-08-02 23:36:01 +0100

    🔴 added kill all current enemies power up

---
+++ MercuryJam/Assets/Scripts/KillAllPowerUp.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillAllPowerUp : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PickUp();
+        }
+    }
+
+    public void PickUp()
+    {
+        SpawnManager.Instance.ClearEnemies();
+        Destroy(gameObject);
+    }
+}
